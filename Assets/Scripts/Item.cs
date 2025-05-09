1a9326665f2ca706cafd5b5376eefdaff6017a2a
MINatoDesuGa <lminlunsims14@gmail.com>
2025-05-09 13:35:21 +0530

    added swipe item UI functionality

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,20 +3,26 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
+using DG.Tweening;
 
-public class Item : MonoBehaviour, IPointerDownHandler, IPointerClickHandler, IPointerUpHandler {
+public class Item : MonoBehaviour, IPointerDownHandler, IPointerClickHandler, IPointerUpHandler, IDragHandler {
     public static event Action<ItemAction, string, ItemData> OnItemAction;
     
     private const float ITEM_VIEW_TRIGGER_DELAY = 0.8f;
+    private const float ITEM_DELETE_TRIGGER_DISTANCE = 100f; // trigger delete distance of item drag
+    private const float ITEM_DELETE_START_THRESHOLD_DISTANCE = 10f; //when to start tweening item for delete
 
     [SerializeField] private TMP_Text _title;
+    [SerializeField] private RectTransform _targetItemTransform;
     //TODO: delete item (on swipe left)
 
     private string _itemTitle;
     private ItemData _itemData;
 
     private bool _canActivateAction = true; // this will be used to not trigger multiple action at once
+    private Vector2 _startTouchPos;
+    private Vector2 _initialItemSize;
+    private float _minDragX;
 
     private WaitForSeconds _delayDuration;
     private Coroutine _delayViewCoroutine;
@@ -26,6 +32,8 @@ public class Item : MonoBehaviour, IPointerDownHandler, IPointerClickHandler, IP
         _itemTitle = _title.text = itemTitle;
 
         _delayDuration = new(ITEM_VIEW_TRIGGER_DELAY);
+        _initialItemSize = _targetItemTransform.rect.size;
+        _minDragX = _initialItemSize.x - ITEM_DELETE_TRIGGER_DISTANCE;
     }
     private void ResetCoroutine() {
         if(_delayViewCoroutine != null) { 
@@ -43,6 +51,11 @@ public class Item : MonoBehaviour, IPointerDownHandler, IPointerClickHandler, IP
     public void OnPointerDown(PointerEventData eventData) {
         //On hold for specified trigger time, trigger item view
         ResetCoroutine();
+        if (_initialItemSize != _targetItemTransform.sizeDelta) {
+            _initialItemSize = _targetItemTransform.sizeDelta;
+            _minDragX = _initialItemSize.x - ITEM_DELETE_TRIGGER_DISTANCE;
+        }
+        _startTouchPos = eventData.position;
         _delayViewCoroutine = StartCoroutine(DelayItemViewTrigger());
 
         IEnumerator DelayItemViewTrigger() {
@@ -53,7 +66,13 @@ public class Item : MonoBehaviour, IPointerDownHandler, IPointerClickHandler, IP
         }
     }
     public void OnPointerUp(PointerEventData eventData) {
+        _targetItemTransform.sizeDelta = _initialItemSize;
         ResetCoroutine(); //on released earler than specified trigger time, reset item view trigger
+    }
+    public void OnDrag(PointerEventData eventData) {
+        _targetItemTransform.sizeDelta = new Vector2 (
+            Mathf.Clamp( _initialItemSize.x - (_startTouchPos.x - eventData.position.x), _minDragX, _initialItemSize.x) 
+            , _initialItemSize.y);
     }
 }
 public enum ItemAction {
